1e93624648408d47d36fd25a4be9ae7401aac927
DrRobotmck <mckenneth.scott@icloud.com>
2013-12-08 16:30:27 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheJoe::Application.config.secret_key_base = '99c2dd3871a56be5e6345d05d18a925971e3bec96a53c6f46141ff42a40f1366a6d0f097c8db30775cefe75237c05d6f1beac54852b8f56d808344e187e88a42'
